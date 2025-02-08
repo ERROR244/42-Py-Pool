f0deea0b472aa882db140c749739ff3aeae9318a
ERROR244 <khalilsohail24@gmail.com>
2025-02-08 12:49:52 +0100

    Done.

---
+++ Training-Piscine-Python-for-datascience-4/ex03/tester1.py
@@ -0,0 +1,7 @@
+from new_student import Student
+
+student = Student(name = "Edward", surname = "agle")
+student1 = Student(name = "Edward", surname = "agle", active=False)
+
+print(student)
+print(student1)
